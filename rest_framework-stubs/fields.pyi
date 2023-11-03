e1a0aead345421c162c5cf99742aefa7a442b3af
anthony sottile <anthony.sottile@sentry.io>
2023-11-03 12:46:32 -0400

    represent choices in ChoiceField as Sequence
    requiring Sequence here means that choices will always be in a deterministic order rather than not due to set / frozenset

--- rest_framework-stubs/fields.pyi
+++ rest_framework-stubs/fields.pyi
@@ -458,7 +458,7 @@ class ChoiceField(Field[str, str | int | tuple[str | int, str | int | tuple], st
     _choices: dict
     def __init__(
         self,
-        choices: Iterable[Any],
+        choices: Sequence[Any],
         *,
         read_only: bool = ...,
         write_only: bool = ...,
@@ -493,7 +493,7 @@ class MultipleChoiceField(
     allow_empty: bool
     def __init__(
         self,
-        choices: Iterable[Any],
+        choices: Sequence[Any],
         read_only: bool = ...,
         write_only: bool = ...,
         required: bool = ...,
